3e1722166278ceb1b32c51e89640c8d98c681554
David.Bewley <David.Bewley@CF247-DEV-L073>
2018-11-09 08:39:14 +0000

    Added the start base API for logic

---
+++ BDDFirstAttempt/TillApi/Controllers/AliveController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TillApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AliveController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
